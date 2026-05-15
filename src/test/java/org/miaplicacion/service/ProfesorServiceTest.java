988f4e4a2e7b531d9b62e57334b4bb626926ef94
Kevin Ras <kevinrassan23@gmail.com>
2026-05-15 19:44:29 +0100

    Test fallido realizado a propósito

--- src/test/java/org/miaplicacion/service/ProfesorServiceTest.java
+++ src/test/java/org/miaplicacion/service/ProfesorServiceTest.java
@@ -37,7 +37,7 @@ public class ProfesorServiceTest {
         when(repository.save(any(Profesor.class))).thenReturn(profesor);
         Profesor guardado = service.guardarProfesor(new Profesor());
         assertNotNull(guardado);
-        assertEquals("Juan Perez", guardado.getNombre());
+        assertEquals("Roberto García", guardado.getNombre());
         verify(repository, times(1)).save(any(Profesor.class));
     }
 
